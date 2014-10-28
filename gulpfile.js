89370767acd88a4f57a160aec21a9adb5a69a8e3
Jan T. Sott <jst1@Paros-Mac-Pro.local>
2014-10-28 14:38:18 +0100

    improved build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+var jshint  = require('gulp-jshint');
+var gulp    = require('gulp');
+var concat  = require('gulp-concat');
+var cssmin  = require('gulp-cssmin');
+
+gulp.task('init',   ['apache', 'fontawesome']);
+gulp.task('make',   ['cssmin']);
 
 gulp.task('lint', function() {
   return gulp.src('feeds.json')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('apache', function () {
+    gulp.src(['node_modules/apache-server-configs/dist/.htaccess'])
+    .pipe(concat('.htaccess'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('cssmin', function() {
+    gulp.src([
+       'src/style.css',
+       'node_modules/font-awesome/css/*.*'
+     ])
+     .pipe(concat('style.min.css'))
+     .pipe(cssmin())
+     .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('fontawesome', function() {
+    gulp.src([
+       'node_modules/font-awesome/fonts/*.*'
+     ])
+     .pipe(gulp.dest('dist/fonts/'));
 });
