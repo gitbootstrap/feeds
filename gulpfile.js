363163211471fa6ebf75c11ef545c8f6e8b42d5e
idleberg <jan@idleberg.com>
2017-01-18 11:05:15 +0100

    update linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,24 @@
-var jshint  = require('gulp-jshint');
-var gulp    = require('gulp');
-var concat  = require('gulp-concat');
-var cssmin  = require('gulp-cssmin');
+ // Dependencies
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+const jsonlint = require('gulp-jsonlint');
 
-gulp.task('init',   ['apache', 'fontawesome']);
-gulp.task('make',   ['cssmin']);
+// Supported files
+const jsonFiles = [
+  'feeds.json'
+];
 
-gulp.task('lint', function() {
-  return gulp.src('feeds.json')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+// Lint JSON
+gulp.task('lint:json', gulp.series(function(done) { 
+  gulp.src(jsonFiles)
+    .pipe(debug({title: 'json-lint'}))
+    .pipe(jsonlint())
+    .pipe(jsonlint.failAfterError())
+    .pipe(jsonlint.reporter());
+  done();
+}));
 
-gulp.task('apache', function () {
-    gulp.src(['node_modules/apache-server-configs/dist/.htaccess'])
-    .pipe(concat('.htaccess'))
-    .pipe(gulp.dest('./'))
-});
-
-gulp.task('cssmin', function() {
-    gulp.src([
-       'src/style.css',
-       'node_modules/font-awesome/css/*.*'
-     ])
-     .pipe(concat('style.min.css'))
-     .pipe(cssmin())
-     .pipe(gulp.dest('dist/css/'));
-});
-
-gulp.task('fontawesome', function() {
-    gulp.src([
-       'node_modules/font-awesome/fonts/*.*'
-     ])
-     .pipe(gulp.dest('dist/fonts/'));
-});
+// Available tasks
+gulp.task('lint', gulp.parallel('lint:json', function(done) {
+  done();
+}));
